fced68817949bcc391fe3310d92f86ff1e5dcbbb
david.s <david.s@lvision.io>
2021-08-20 12:40:38 +0300

    second v

--- hello_python/__init__.py
+++ hello_python/__init__.py
@@ -1,2 +1,3 @@
 def _cli():
     print('hello python')
+# https://gist.github.com/zancas/6742c75f9ebc9a76f247
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     # package name
     name='hello-python-derldalfor100',
     # package version
-    version='0.1.0',
+    version='0.2.0',
     # short description
     description='A script that prints "hello python"',
     # the documentation displayed on pypi
